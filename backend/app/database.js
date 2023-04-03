e1253f0e44711718c4291783545afc69cc334a03
MiqueiasAlves <mica_rox@hotmail.com>
2023-04-03 18:50:46 -0300

    Adjusting db name

--- backend/app/database.js
+++ backend/app/database.js
@@ -1,5 +1,5 @@
 // Database
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('teste_estagio_ii', process.env.db_user, process.env.db_pass, {dialect: 'mysql', host: 'localhost'});
+const sequelize = new Sequelize(process.env.db_name, process.env.db_user, process.env.db_pass, {dialect: 'mysql', host: 'localhost'});
 
 module.exports = sequelize;
--- backend/app/server.js
+++ backend/app/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-require('dotenv-safe').config();
+require('dotenv-safe').config({
+    allowEmptyValues: true
+});
 const app = express();
 const port = process.env.port;
 
