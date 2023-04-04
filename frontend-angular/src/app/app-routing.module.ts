9c47db43b01760db12b6c075c0f44f1fbd60763a
MiqueiasAlves <mica_rox@hotmail.com>
2023-04-03 23:18:48 -0300

    Added Homepage

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -6,11 +6,10 @@ import { InfoPageComponent } from './components/info-page/info-page.component';
 import { PreencherFormularioComponent } from './components/preencher-formulario/preencher-formulario.component';
 
 const routes: Routes = [
-  {path: '', component: InfoPageComponent},
-  {path: 'homelol', component: HomePageComponent},
+  {path: '', component: HomePageComponent},
+  {path: 'info', component: InfoPageComponent},
   {path: 'criar-form', component: CriarFormularioComponent},
   {path: 'preencher-form', component: PreencherFormularioComponent},
-
 ];
 
 @NgModule({
